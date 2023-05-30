17cab5998e46f4e6c6dd40a589aa9a37c0d00c53
isaacbull <117191513+isaacbull@users.noreply.github.com>
2023-05-30 13:54:47 -0400

    adding new methods to run command line on a python program and using sys args

---
+++ using_sys_argv.py
@@ -0,0 +1,8 @@
+import sys
+
+print("This is the name of the script:", sys.argv[0])
+print("Number of arguments:", len(sys.argv))
+print("The arguments are:", str(sys.argv))
+
+
+# type this in terminal to run it : python3 using_sys_argv.py arg1 arg2 arg3
